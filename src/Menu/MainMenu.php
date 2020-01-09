9219ead4153c858ad3b176577bb2d1cdd5682f4b
Artem Ryzhkov <digital_boy@mail.ru>
2020-01-09 21:27:38 +0700

    comeback user geoposition

--- src/Menu/MainMenu.php
+++ src/Menu/MainMenu.php
@@ -71,7 +71,7 @@ class MainMenu
         $menu->addChild('Cooperatives',     ['route' => 'account_coop']);
         $menu->addChild('Profile',          ['route' => 'account_profile']);
         $menu->addChild('Telegram',         ['route' => 'account_telegram']);
-        //$menu->addChild('Geoposition',      ['route' => 'account_geoposition']);
+        $menu->addChild('Geoposition',      ['route' => 'account_geoposition']);
         $menu->addChild('Change password',  ['route' => 'account_password']);
 
         return $menu;
