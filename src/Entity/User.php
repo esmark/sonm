97d7d43aea7820a8514aa05b6a27cdd1730c09f8
artem <digital_boy@mail.ru>
2020-02-07 01:07:14 +0700

    small refactor

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,12 +5,12 @@ declare(strict_types=1);
 namespace App\Entity;
 
 use App\DTO\Worksheet;
+use App\Model\UserModel;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Smart\CoreBundle\Doctrine\ColumnTrait;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
@@ -37,69 +37,11 @@ use Symfony\Component\Validator\Constraints as Assert;
  *     message="Email is already exists"
  * )
  */
-class User implements UserInterface
+class User extends UserModel
 {
     use ColumnTrait\Uuid;
-    use ColumnTrait\EmailUnique;
     use ColumnTrait\CreatedAt;
     use ColumnTrait\IsEnabled;
-
-    const ROLE_DEFAULT     = 'ROLE_USER';
-    const ROLE_SUPER_ADMIN = 'ROLE_SUPER_ADMIN';
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=40)
-     * @Assert\Length(min = 3, minMessage = "Username length must be at least {{ limit }} characters long")
-     * @Assert\NotNull(message="This value is not valid.")
-     */
-    protected $username;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=40, unique=true)
-     */
-    protected $username_canonical;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=100)
-     * @Assert\NotBlank()
-     * @Assert\Email(mode="strict")
-     */
-    protected $email;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=100, unique=true)
-     */
-    protected $email_canonical;
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(type="string", length=190)
-     * @Assert\Length(min = 6, minMessage = "Password length must be at least {{ limit }} characters long")
-     */
-    protected $password;
-
-    /**
-     * @var string|null
-     *
-     * Plain password. Used for model validation. Must not be persisted.
-     */
-    protected $plain_password;
-
-    /**
-     * @var array
-     *
-     * @ORM\Column(type="array")
-     */
-    protected $roles;
 
     /**
      * Имя
@@ -226,13 +168,6 @@ class User implements UserInterface
     protected $has_relative;
 
     /**
-     * @var \DateTime|null
-     *
-     * @ORM\Column(type="datetime", nullable=true)
-     */
-    protected $last_login;
-
-    /**
      * @var float|null
      *
      * @ORM\Column(type="decimal", precision=14, scale=11, nullable=true)
@@ -342,7 +277,8 @@ class User implements UserInterface
      */
     public function __construct()
     {
-        $this->created_at   = new \DateTime();
+        parent::__construct();
+
         $this->is_enabled   = true;
         $this->has_relative = false;
         $this->email        = '';
@@ -351,9 +287,6 @@ class User implements UserInterface
         $this->groups       = new ArrayCollection();
         $this->oauths       = new ArrayCollection();
         $this->payments     = new ArrayCollection();
-        $this->password     = '';
-        $this->roles        = [];
-        $this->username     = '';
     }
 
     /**
@@ -386,260 +319,6 @@ class User implements UserInterface
         }
 
         return false;
-    }
-
-    /**
-     * @return string
-     */
-    public function serialize(): string
-    {
-        return serialize([
-            $this->id,
-            $this->is_enabled,
-            $this->email,
-            $this->email_canonical,
-            $this->username,
-            $this->username_canonical,
-            $this->password
-        ]);
-    }
-
-    /**
-     * @param string $serialized
-     */
-    public function unserialize($serialized): void
-    {
-        [
-            $this->id,
-            $this->is_enabled,
-            $this->email,
-            $this->email_canonical,
-            $this->username,
-            $this->username_canonical,
-            $this->password
-        ] = unserialize($serialized, ['allowed_classes' => false]);
-    }
-
-    /**
-     * @param string $string
-     *
-     * @return string|null
-     */
-    static public function canonicalize(string $string): ?string
-    {
-        if (null === $string) {
-            return null;
-        }
-
-        $encoding = mb_detect_encoding($string);
-        $result = $encoding
-            ? mb_convert_case($string, MB_CASE_LOWER, $encoding)
-            : mb_convert_case($string, MB_CASE_LOWER);
-
-        return $result;
-    }
-
-    /**
-     * Returns the salt that was originally used to encode the password.
-     *
-     * {@inheritdoc}
-     */
-    public function getSalt(): ?string
-    {
-        // See "Do you need to use a Salt?" at https://symfony.com/doc/current/cookbook/security/entity_provider.html
-        // we're using bcrypt in security.yml to encode the password, so
-        // the salt value is built-in and you don't have to generate one
-
-        return null;
-    }
-
-    /**
-     * Removes sensitive data from the user.
-     *
-     * {@inheritdoc}
-     */
-    public function eraseCredentials(): void
-    {
-        // if you had a plainPassword property, you'd nullify it here
-        $this->plain_password = null;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isSuperAdmin(): bool
-    {
-        return $this->hasRole(static::ROLE_SUPER_ADMIN);
-    }
-
-    /**
-     * @param bool $boolean
-     *
-     * @return $this
-     */
-    public function setSuperAdmin(bool $boolean): self
-    {
-        if (true === $boolean) {
-            $this->addRole(static::ROLE_SUPER_ADMIN);
-        } else {
-            $this->removeRole(static::ROLE_SUPER_ADMIN);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUsername(): string
-    {
-        return $this->username;
-    }
-
-    /**
-     * @param string $username
-     *
-     * @return $this
-     */
-    public function setUsername(string $username): self
-    {
-        $this->username = trim($username);
-        $this->username_canonical = self::canonicalize($this->username);
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getUsernameCanonical(): string
-    {
-        return $this->username_canonical;
-    }
-
-    /**
-     * @param string $username_canonical
-     *
-     * @return $this
-     */
-    public function setUsernameCanonical(string $username_canonical): self
-    {
-        $this->username_canonical = $username_canonical;
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getPassword(): string
-    {
-        return $this->password;
-    }
-
-    /**
-     * @param string $password
-     *
-     * @return $this
-     */
-    public function setPassword(string $password): self
-    {
-        $this->password = $password;
-
-        return $this;
-    }
-
-    /**
-     * @return string|null
-     */
-    public function getPlainPassword(): ?string
-    {
-        return $this->plain_password;
-    }
-
-    /**
-     * @param string|null $plain_password
-     *
-     * @return $this
-     */
-    public function setPlainPassword(?string $plain_password): self
-    {
-        $this->plain_password = $plain_password;
-
-        return $this;
-    }
-
-    /**
-     * @param string $role
-     *
-     * @return $this
-     */
-    public function addRole(string $role): self
-    {
-        $role = strtoupper($role);
-        if ($role === static::ROLE_DEFAULT) {
-            return $this;
-        }
-
-        if (!in_array($role, $this->roles, true)) {
-            $this->roles[] = $role;
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param string $role
-     *
-     * @return $this
-     */
-    public function removeRole(string $role): self
-    {
-        if (false !== $key = array_search(strtoupper($role), $this->roles, true)) {
-            unset($this->roles[$key]);
-            $this->roles = array_values($this->roles);
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param string $role
-     *
-     * @return bool
-     */
-    public function hasRole(string $role): bool
-    {
-        return in_array(strtoupper($role), $this->getRoles(), true);
-    }
-
-    /**
-     * Returns the roles or permissions granted to the user for security.
-     *
-     * @return array
-     */
-    public function getRoles(): array
-    {
-        $roles = $this->roles;
-
-        // guarantees that a user always has at least one role for security
-        if (empty($roles)) {
-            $roles[] = 'ROLE_USER';
-        }
-
-        return array_unique($roles);
-    }
-
-    /**
-     * @param array $roles
-     *
-     * @return $this
-     */
-    public function setRoles(array $roles): self
-    {
-        $this->roles = $roles;
-
-        return $this;
     }
 
     /**
@@ -723,26 +402,6 @@ class User implements UserInterface
     }
 
     /**
-     * @return \DateTime|null
-     */
-    public function getLastLogin(): ?\DateTime
-    {
-        return $this->last_login;
-    }
-
-    /**
-     * @param \DateTime|null $last_login
-     *
-     * @return $this
-     */
-    public function setLastLogin(?\DateTime $last_login): self
-    {
-        $this->last_login = $last_login;
-
-        return $this;
-    }
-
-    /**
      * @return int|null
      */
     public function getTelegramUserId(): ?int
@@ -782,46 +441,6 @@ class User implements UserInterface
         return $this;
     }
 
-    /**
-     * @return string
-     */
-    public function getEmail(): string
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param string $email
-     *
-     * @return $this
-     */
-    public function setEmail(string $email): self
-    {
-        $this->email = trim($email);
-        $this->email_canonical = self::canonicalize(trim($email));
-
-        return $this;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEmailCanonical(): string
-    {
-        return $this->email_canonical;
-    }
-
-    /**
-     * @param string $email_canonical
-     *
-     * @return $this
-     */
-    public function setEmailCanonical(string $email_canonical): self
-    {
-        $this->email_canonical = $email_canonical;
-
-        return $this;
-    }
     /**
      * @return UserOauth[]|ArrayCollection
      */
