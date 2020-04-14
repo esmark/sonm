517f467395505c9266021c029ba02f80c4bceb23
artem <digital_boy@mail.ru>
2020-04-14 22:02:11 +0700

    fix

--- src-vendor/JointPurchaseBundle/Form/Type/JointPurchaseProductFormType.php
+++ src-vendor/JointPurchaseBundle/Form/Type/JointPurchaseProductFormType.php
@@ -40,7 +40,7 @@ class JointPurchaseProductFormType extends AbstractType
             ])
 
             ->add('create', SubmitType::class, ['attr' => ['class' => 'btn-success']])
-            ->add('update', SubmitType::class, ['attr' => ['class' => 'btn-success']])
+            ->add('update', SubmitType::class, ['attr' => ['class' => 'btn-success'], 'label' => 'Save'])
             ->add('cancel', SubmitType::class, ['attr' => ['class' => 'btn-light', 'formnovalidate' => 'formnovalidate']])
         ;
     }
